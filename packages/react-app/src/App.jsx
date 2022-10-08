19fdb9c295c2531942ec5f9ae826b24230857e81
nerzhul250 <sebastian.arango@correo.icesi.edu.co>
2022-10-08 05:48:40 -0500

    raw web3auth login

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,33 +1,187 @@
 import "antd/dist/antd.css";
+import React, { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/web3auth";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
 import { useBalance } from "eth-hooks";
-import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import { ThemeSwitch } from "./components";
 import { NETWORKS } from "./constants";
 import { SignIn, Pool } from "./views";
 import { useStaticJsonRPC } from "./hooks";
+import RPC from "./hooks/web3RPC";
+
+const clientId = "BFt8f0zCLJtfThnpTEsmOs8EdgHgmIkKsObguZbdp7XEEwheU4BCDH8shgSBQ3l-UVTKV0qqrLIEnsXSYe35kpE"; // get from https://dashboard.web3auth.io
 
 /// 📡 What chain are your contracts deployed to?
 const initialNetwork = NETWORKS.localhost; // Select your target frontend network (localhost, goerli, xdai, mainnet)
 
 function App(props) {
-  const networkOptions = [initialNetwork.name, "mainnet", "goerli"];
-  const [address] = useState();
-  const [selectedNetwork] = useState(networkOptions[0]);
-  const targetNetwork = NETWORKS[selectedNetwork];
-  const localProvider = useStaticJsonRPC([
-    process.env.REACT_APP_PROVIDER ? process.env.REACT_APP_PROVIDER : targetNetwork.rpcUrl,
-  ]);
-  const yourLocalBalance = useBalance(localProvider, address);
+  const [web3auth, setWeb3auth] = useState();
+  const [provider, setProvider] = useState();
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x13881", // polygon mumbai chain-id in hex
+            rpcTarget: "https://polygon-mumbai.g.alchemy.com/v2/RGPhWsJCplbShwpSYOo1Df7oplSaTl8d", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+        });
+
+        setWeb3auth(web3auth);
+
+        await web3auth.initModal();
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    console.log("web3auth is already initialized");
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    console.log(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
+
+  const getChainId = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const chainId = await rpc.getChainId();
+    console.log(chainId);
+  };
+  const getAccounts = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    console.log(address);
+  };
+
+  let yourLocalBalance = 0;
+
+  const getBalance = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    yourLocalBalance = balance;
+    console.log(balance);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    console.log(receipt);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    console.log(signedMessage);
+  };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const privateKey = await rpc.getPrivateKey();
+    console.log(privateKey);
+  };
+  const loggedInView = (
+    <>
+      <button onClick={getUserInfo} className="card">
+        Get User Info
+      </button>
+      <button onClick={getChainId} className="card">
+        Get Chain ID
+      </button>
+      <button onClick={getAccounts} className="card">
+        Get Accounts
+      </button>
+      <button onClick={getBalance} className="card">
+        Get Balance
+      </button>
+      <button onClick={sendTransaction} className="card">
+        Send Transaction
+      </button>
+      <button onClick={signMessage} className="card">
+        Sign Message
+      </button>
+      <button onClick={getPrivateKey} className="card">
+        Get Private Key
+      </button>
+      <button onClick={logout} className="card">
+        Log Out
+      </button>
+
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
-          <SignIn yourLocalBalance={yourLocalBalance} />
+          <div className="grid">{provider ? loggedInView : unloggedInView}</div>
         </Route>
         <Route exact path="/pool">
-          <Pool yourLocalBalance={yourLocalBalance} />
+          <div className="grid">{provider ? <Pool yourLocalBalance={yourLocalBalance} /> : unloggedInView}</div>
         </Route>
       </Switch>
       <ThemeSwitch />
