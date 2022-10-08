a234289ea9d1d1d6c708cb4b8ab1b46bfea086a1
Miguel Jiménez <1284036+jachinte@users.noreply.github.com>
2022-10-08 15:10:07 -0400

    Organize sign in code

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -2,16 +2,24 @@ import "antd/dist/antd.css";
 import React, { useEffect, useState } from "react";
 import { Web3Auth } from "@web3auth/web3auth";
 import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { SignIn, Home, Pool, Invitation, Contribution, New, Confirmation } from "./views";
 import { CLIENT_ID, MUMBAI_CHAIN_ID, ALCHEMY_KEY } from "./constants";
-import RPC from "./hooks/web3RPC";
 import "./App.css";
 
 function App(props) {
+  const history = useHistory();
   const [web3auth, setWeb3auth] = useState();
   const [provider, setProvider] = useState();
   const [username, setUsername] = useState();
+
+  const getUserInfo = async () => {
+    if (web3auth) {
+      const user = await web3auth.getUserInfo();
+      return user;
+    }
+    console.error("web3auth not initialized yet");
+  };
 
   useEffect(() => {
     const init = async () => {
@@ -30,7 +38,7 @@ function App(props) {
         setWeb3auth(web3auth);
 
         await web3auth.initModal();
-        if (typeof web3auth.provider !== undefined) {
+        if (web3auth.provider) {
           setProvider(web3auth.provider);
           const userInfo = await getUserInfo();
           setUsername(userInfo.name);
@@ -43,120 +51,21 @@ function App(props) {
     init();
   }, [provider]);
 
-  const getUserInfo = async () => {
-    if (typeof web3auth === undefined) {
-      console.error("web3auth not initialized yet");
-      return;
-    }
-    const user = await web3auth.getUserInfo();
-    console.log(user);
-    return user;
-  };
-
   const logout = async () => {
-    if (typeof web3auth === undefined) {
+    if (!web3auth) {
       console.error("web3auth not initialized yet");
       return;
     }
     await web3auth.logout();
     setProvider(null);
+    history.push("/");
   };
-
-  const getChainId = async () => {
-    if (typeof provider === undefined) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const chainId = await RPC.getChainId(provider);
-    console.log(chainId);
-  };
-  const getAccounts = async () => {
-    if (typeof provider === undefined) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const address = await RPC.getAccounts(provider);
-    console.log(address);
-  };
-
-  let yourLocalBalance = 0;
-
-  const getBalance = async () => {
-    if (typeof provider === undefined) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const balance = await RPC.getBalance(provider);
-    yourLocalBalance = balance;
-    console.log(balance);
-  };
-
-  const sendTransaction = async () => {
-    if (typeof provider === undefined) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const receipt = await RPC.sendTransaction(provider);
-    console.log(receipt);
-  };
-
-  const signMessage = async () => {
-    if (typeof provider === undefined) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const signedMessage = await RPC.signMessage(provider);
-    console.log(signedMessage);
-  };
-
-  const getPrivateKey = async () => {
-    if (typeof provider === undefined) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const privateKey = await RPC.getPrivateKey(provider);
-    console.log(privateKey);
-  };
-  const loggedInView = (
-    <>
-      <button onClick={getUserInfo} className="card">
-        Get User Info
-      </button>
-      <button onClick={getChainId} className="card">
-        Get Chain ID
-      </button>
-      <button onClick={getAccounts} className="card">
-        Get Accounts
-      </button>
-      <button onClick={getBalance} className="card">
-        Get Balance
-      </button>
-      <button onClick={sendTransaction} className="card">
-        Send Transaction
-      </button>
-      <button onClick={signMessage} className="card">
-        Sign Message
-      </button>
-      <button onClick={getPrivateKey} className="card">
-        Get Private Key
-      </button>
-      <button onClick={logout} className="card">
-        Log Out
-      </button>
-
-      <div id="console" style={{ whiteSpace: "pre-line" }}>
-        <p style={{ whiteSpace: "pre-line" }}></p>
-      </div>
-    </>
-  );
-
-  const unloggedInview = <SignIn web3auth={web3auth} provider={provider} setProvider={setProvider} />;
 
   const auth = View => {
-    if (typeof provider === undefined || typeof username === undefined) {
-      return <SignIn web3auth={web3auth} provider={provider} setProvider={setProvider} />;
-    } else {
+    if (provider && username) {
       return View;
+    } else {
+      return <SignIn web3auth={web3auth} provider={provider} setProvider={setProvider} />;
     }
   };
 
@@ -170,8 +79,7 @@ function App(props) {
           <Invitation author={"Maria"} />
         </Route>
         <Route exact path="/home">
-          {provider && username ? <Home username={username} provider={provider} logout={logout} /> : unloggedInview}
-          {/* {auth(<Home username={username} provider={provider} logout={logout} />)} */}
+          {auth(<Home username={username} provider={provider} logout={logout} />)}
         </Route>
         <Route path="/new">{auth(<New />)}</Route>
         <Route path="/pool/:id">{auth(<Pool />)}</Route>
